0ba44039b5ad990e85678101ffef610fa5cf63ba
Fredrik Hagen Fasteraune <frha@dips.no>
2023-11-28 10:10:42 +0100

    update LSP

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -41,10 +41,10 @@ require("lspconfig").pylsp.setup({
     capabilities = capabilities,
 })
 
-require("lspconfig")["denols"].setup({
-    on_attach = on_attach,
-    capabilities = capabilities,
-})
+-- require("lspconfig")["denols"].setup({
+--     on_attach = on_attach,
+--     capabilities = capabilities,
+-- })
 
 -- require("lspconfig")["csharp_ls"].setup({
 --     on_attach = on_attach,
@@ -70,11 +70,11 @@ require("lspconfig")["omnisharp"].setup({
     enable_ms_build_load_projects_on_demand = false,
 
     -- Enables support for roslyn analyzers, code fixes and rulesets.
-    enable_roslyn_analyzers = false,
+    enable_roslyn_analyzers = true,
 
     -- Specifies whether 'using' directives should be grouped and sorted during
     -- document formatting.
-    organize_imports_on_format = false,
+    organize_imports_on_format = true,
 
     -- Enables support for showing unimported types and unimported extension
     -- methods in completion lists. When committed, the appropriate using
@@ -82,7 +82,7 @@ require("lspconfig")["omnisharp"].setup({
     -- have a negative impact on initial completion responsiveness,
     -- particularly for the first few completion sessions after opening a
     -- solution.
-    enable_import_completion = false,
+    enable_import_completion = true,
 
     -- Specifies whether to include preview versions of the .NET SDK when
     -- determining which version to use for project loading.
