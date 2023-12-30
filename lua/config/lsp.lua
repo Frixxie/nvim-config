f9a3bfb394b53ea7740d26a83bec5d28a25a769f
Fredrik <freddahf@outlook.com>
2023-12-30 14:22:19 +0100

    add some lsp's which i use by default

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -27,8 +27,48 @@ end
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 require("mason").setup()
-require("mason-lspconfig").setup()
+require("mason-lspconfig").setup({
+    ensure_installed = {
+        "lua_ls",
+        "rust_analyzer",
+        "omnisharp",
+        "clangd",
+        "hls",
+        "denols",
+        "svelte",
+        "dockerfile-language-server",
+        "docker-compose-language-server"
+    }
+})
 
+require("lspconfig")["dockerfile-language-server"].setup({
+    capabilities = capabilities,
+    on_attach = on_attach,
+})
+require("lspconfig")["docker-compose-language-server"].setup({
+    capabilities = capabilities,
+    on_attach = on_attach,
+})
+require("lspconfig")["clangd"].setup({
+    capabilities = capabilities,
+    on_attach = on_attach,
+})
+require("lspconfig")["denols"].setup({
+    capabilities = capabilities,
+    on_attach = on_attach,
+})
+require("lspconfig")["svelte"].setup({
+    capabilities = capabilities,
+    on_attach = on_attach,
+})
+require("lspconfig")["hls"].setup({
+    capabilities = capabilities,
+    on_attach = on_attach,
+})
+require("lspconfig")["lua_ls"].setup({
+    capabilities = capabilities,
+    on_attach = on_attach,
+})
 require("lspconfig")["omnisharp"].setup({
     capabilities = capabilities,
     on_attach = on_attach,
@@ -37,134 +77,3 @@ require("lspconfig")["rust_analyzer"].setup({
     capabilities = capabilities,
     on_attach = on_attach,
 })
-
--- require("lspconfig").pylsp.setup({
---     settings = {
---         pylsp = {
---             plugins = {
---                 pycodesyle = {
---                     ignore = { "W391" },
---                     maxLineLength = 100,
---                 },
---             },
---         },
---     },
---     on_attach = on_attach,
---     capabilities = capabilities,
--- })
-
--- require("lspconfig")["denols"].setup({
---     on_attach = on_attach,
---     capabilities = capabilities,
--- })
-
--- require("lspconfig")["csharp_ls"].setup({
---     on_attach = on_attach,
---     capabilities = capabilities,
---     --     filetypes = {"cs", "csx"}
--- })
--- require("lspconfig")["omnisharp"].setup({
---     on_attach = on_attach,
---     capabilities = capabilities,
---     cmd = { "dotnet", "/Users/fredrik/opts/omnisharp-osx-arm64-net6.0/OmniSharp.dll" },
-
---     -- Enables support for reading code style, naming convention and analyzer
---     -- settings from .editorconfig.
---     enable_editorconfig_support = false,
-
---     -- If true, MSBuild project system will only load projects for files that
---     -- were opened in the editor. This setting is useful for big C# codebases
---     -- and allows for faster initialization of code navigation features only
---     -- for projects that are relevant to code that is being edited. With this
---     -- setting enabled OmniSharp may load fewer projects and may thus display
---     -- incomplete reference lists for symbols.
---     enable_ms_build_load_projects_on_demand = false,
-
---     -- Enables support for roslyn analyzers, code fixes and rulesets.
---     enable_roslyn_analyzers = true,
-
---     -- Specifies whether 'using' directives should be grouped and sorted during
---     -- document formatting.
---     organize_imports_on_format = true,
-
---     -- Enables support for showing unimported types and unimported extension
---     -- methods in completion lists. When committed, the appropriate using
---     -- directive will be added at the top of the current file. This option can
---     -- have a negative impact on initial completion responsiveness,
---     -- particularly for the first few completion sessions after opening a
---     -- solution.
---     enable_import_completion = true,
-
---     -- Specifies whether to include preview versions of the .NET SDK when
---     -- determining which version to use for project loading.
---     sdk_include_prereleases = true,
-
---     -- Only run analyzers against open files when 'enableRoslynAnalyzers' is
---     -- true
---     analyze_open_documents_only = false,
--- })
--- require("lspconfig")["texlab"].setup({
---     on_attach = on_attach,
---     capabilities = capabilities,
--- })
--- -- require("lspconfig")["grammarly"].setup({
--- --     on_attach = on_attach,
--- --     filetypes = { "tex", "markdown" },
--- --     capabilities = capabilities,
--- -- })
--- require("lspconfig")["marksman"].setup({
---     on_attach = on_attach,
---     capabilities = capabilities,
--- })
--- require("lspconfig")["clangd"].setup({
---     on_attach = on_attach,
---     capabilities = capabilities,
--- })
--- require("lspconfig")["svelte"].setup({
---     on_attach = on_attach,
---     capabilities = capabilities,
--- })
--- require("lspconfig")["tsserver"].setup({
---     on_attach = on_attach,
---     capabilities = capabilities,
--- })
--- require("lspconfig")["rust_analyzer"].setup({
---     on_attach = on_attach,
---     capabilities = capabilities,
--- })
--- require("lspconfig")["hls"].setup({
---     on_attach = on_attach,
---     capabilities = capabilities,
--- })
--- require("lspconfig")["fsautocomplete"].setup({
---     on_attach = on_attach,
---     capabilities = capabilities,
--- })
--- require("lspconfig")["tailwindcss"].setup({
---     on_attach = on_attach,
---     capabilities = capabilities,
--- })
--- require 'lspconfig'.lua_ls.setup {
---     settings = {
---         Lua = {
---             runtime = {
---                 -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
---                 version = 'LuaJIT',
---             },
---             diagnostics = {
---                 -- Get the language server to recognize the `vim` global
---                 globals = { 'vim' },
---             },
---             workspace = {
---                 -- Make the server aware of Neovim runtime files
---                 library = vim.api.nvim_get_runtime_file("", true),
---             },
---             -- Do not send telemetry data containing a randomized but unique identifier
---             telemetry = {
---                 enable = false,
---             },
---         },
---     },
---     on_attach = on_attach,
---     capabilities = capabilities,
--- }
