053c297b5b98d28b65321d0825b69666592685b8
Fredrik <freddahf@outlook.com>
2024-01-11 20:45:26 +0100

    fix the annoying keybinds

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -45,7 +45,6 @@ require("telescope").setup({
 
 local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<leader>p", builtin.find_files, {})
-vim.keymap.set("n", "<leader>gp", builtin.git_files, {})
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
 vim.keymap.set("n", "<leader>fm", builtin.marks, {})
