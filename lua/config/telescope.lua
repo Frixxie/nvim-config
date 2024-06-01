e2b62e1806ac562b15e1621162eb4f34efeac963
Fredrik <freddahf@outlook.com>
2024-06-01 11:16:37 +0200

    fix config

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -1,4 +1,13 @@
-require("telescope").setup()
+require("telescope").setup({
+    defaults = {
+        layout_strategy = 'horizontal',
+        sorting_strategy = 'ascending',
+        layout_config = {
+            prompt_position = 'top'
+        }
+
+    }
+})
 require("telescope").load_extension('fzf')
 
 
