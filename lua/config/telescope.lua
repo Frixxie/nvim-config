c8fd8d52715bea827a27ace07094bfe570fea88e
Fredrik Hagen Fasteraune <frha@dips.no>
2025-11-05 08:17:02 +0100

    update deps

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -12,7 +12,7 @@ require("telescope").load_extension('fzf')
 
 
 local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>p", builtin.find_files, {})
+vim.keymap.set("n", "<leader>p", builtin.fd, {})
 vim.keymap.set("n", "<leader>fo", builtin.oldfiles, {})
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
